9ce301177457a79ec9778737618a12132642ec2e
jonathanahern <modestfool@gmail.com>
2020-11-10 21:42:00 -0500

    Add email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
     def welcome_email
         @user = params[:user]
         @url  = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+        mail(to: 'jonathanahernnyc@gmail.com', subject: 'Welcome to My Awesome Site')
     end
 end
