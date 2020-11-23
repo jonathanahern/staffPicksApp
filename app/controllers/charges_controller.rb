fa18df30769c84dfbede52868eb38b1df2c94e3f
jonathanahern <modestfool@gmail.com>
2020-11-22 23:05:23 -0500

    Recurring charges work

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,10 @@
-class​ ​ChargesController < ShopifyApp::AuthenticatedController
+class ChargesController < ShopifyApp::AuthenticatedController
 
-    include​ ​ApplicationHelper
-
-    before_action ​:set_shop
-    
     def activate_charge
         recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(request.params['charge_id'])
-        recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : redirect(root_path)
+        recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : redirect_to(root_path)
         redirect_to(root_path)
     end
 
 end
+
