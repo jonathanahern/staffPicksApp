7cf381a949b95b62c4928d9aa073c88c4af66348
jonathanahern <modestfool@gmail.com>
2020-12-01 23:42:07 -0500

    Add commas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,11 +99,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com'
-    port: 587
-    domain: 'gmail.com'
-    authentication: 'plain'
-    enable_starttls_auto: true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
     user_name: 'staffpicksapp@gmail.com',
     password: ENV['gmail_password']
   }
