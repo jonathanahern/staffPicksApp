d8508bac9b0cf2a788fb05bdfe8c5adc10298715
jonathanahern <modestfool@gmail.com>
2020-12-01 23:36:43 -0500

    Change email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer_default_url_options = { host: 'https://your-app-site.com' }
-  Rails.application.routes.default_url_options[:host] = 'https://your-app-site.com'
+  config.action_mailer_default_url_options = { host: 'https://staff-picks-app.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://staff-picks-app.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
